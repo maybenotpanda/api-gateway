23c82cbd1ad77102960a82e98eefe02c125683e3
maybenotpanda <maybenotpanda@gmail.com>
2025-11-14 09:14:01 +0700

    [middlewares]: add new fn permission

--- routes/courses.js
+++ routes/courses.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const courseHandler = require('./handler/courses')
 
 const verifyToken = require('../middlewares/verifyToken')
+const can = require('../middlewares/permission')
 
-router.post("/", verifyToken, courseHandler.create);
+router.post("/", verifyToken, can('admin'), courseHandler.create);
 
 router.get("/", courseHandler.getAll);
 router.get("/:id", courseHandler.get);
 
-router.put("/:id", verifyToken, courseHandler.update);
-router.delete("/:id", verifyToken, courseHandler.destroy);
+router.put("/:id", verifyToken, can('admin'), courseHandler.update);
+router.delete("/:id", verifyToken, can('admin'), courseHandler.destroy);
 
 module.exports = router;
